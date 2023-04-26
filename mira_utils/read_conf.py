ca01c4818cee10f80aa38806a413dd614c42db4d
michelescarlato <michele.scarlato@gmail.com>
2023-04-26 16:38:59 +0200

    Added read_conf method for fetching data.

--- mira_utils/read_conf.py
+++ mira_utils/read_conf.py
@@ -8,5 +8,20 @@ def read_conf_insert_data(conf_file):
     my_url = config['questdb.parameters']['url']
     secs_interval = config['questdb.parameters']['secs_interval']
     table = config['questdb.parameters']['table']
+    dbname = config['questdb.parameters']['dbname']
 
-    return my_url, secs_interval, table #my_bucket, my_org, my_token,
+    return my_url, secs_interval, table, dbname
+
+def read_conf_fetch_data(conf_file):
+    config = configparser.ConfigParser()
+    config.read(conf_file)
+    config.sections()
+    # Store the URL of your InfluxDB instance
+    my_url = config['questdb.parameters']['url']
+    user = config['questdb.parameters']['user']
+    password = config['questdb.parameters']['password']
+    table = config['questdb.parameters']['table']
+    dbname = config['questdb.parameters']['dbname']
+    port = config['questdb.parameters']['port']
+
+    return my_url, user, table, dbname, password, port
