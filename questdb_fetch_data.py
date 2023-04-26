bc50c0e7f40a9134139f15c0c451af2e2af41d39
michelescarlato <michele.scarlato@gmail.com>
2023-04-26 16:40:38 +0200

    Added logging and starting to parameterize query.

--- questdb_fetch_data.py
+++ questdb_fetch_data.py
@@ -1,18 +1,36 @@
+import logging
+import sys
 import psycopg as pg
+from datetime import datetime
 import time
+import socket
+from mira_utils import read_conf,convert_seconds
+
+log_name = str(datetime.now())
+log_name = log_name.replace(" ","_")
+hostname = str(socket.gethostname())
+print(log_name)
+logging.basicConfig(filename="logs/Fetch_data_"+hostname+"_"+str(log_name)+"_run.log", level=logging.INFO)
+
+file_name = sys.argv[1]
+url, user, table, dbname, password, port = read_conf.read_conf_fetch_data(file_name)
+# start script temporizer
+start = time.time()
 
 # Connect to an existing QuestDB instance
-
-conn_str = 'user=admin password=quest host=127.0.0.1 port=8812 dbname=qdb'
+conn_str = f'user={user} password={password} host={url} port={port} dbname={dbname}'
 with pg.connect(conn_str, autocommit=True) as connection:
     # Open a cursor to perform database operations
-
     with connection.cursor() as cur:
         # Query the database and obtain data as Python objects.
-
-        cur.execute('SELECT * FROM sensors_data WHERE timems BETWEEN \'2023-03-29T00:00:23.000000Z\' AND \'2023-04-25T00:00:23.500000Z\';')
+        cur.execute(f'SELECT * FROM {table} WHERE timems BETWEEN \'2023-03-29T00:00:23.000000Z\' AND \'2023-04-25T00:00:23.500000Z\';')
         records = cur.fetchall()
-        for row in records:
-            print(row)
 
 # the connection is now closed
+
+end = time.time()
+elapsed_time = end - start
+minutes = convert_seconds.convert_seconds(elapsed_time)
+print("Total time elapsed:")
+print(minutes)
+logging.info("Total time elapsed (hh:mm:ss): "+str(minutes))
